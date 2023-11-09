f3802cb4280e6533614bfb46b6b6a8daab18a918
javier-camilo <camiloviloriatovar@gmail.com>
2023-11-09 01:26:15 -0500

    nuevos cambios

--- SOFTWARE/ClientApp/src/app/AppModule.ts
+++ SOFTWARE/ClientApp/src/app/AppModule.ts
@@ -3,20 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { CitasComponent } from './Solicitante/citas/citas.component';
 import { EstadoColaComponent } from './Solicitante/estado-cola/estado-cola.component';
 import { TicketComponent } from './solicitante/ticket/ticket.component';
@@ -27,15 +25,15 @@ import { ConsultarhorarioComponent } from './administrador/gestionhorario/consul
 import { GenrarreporteComponent } from './administrador/genrarreporte/genrarreporte.component';
 import { AtenderCitaComponent } from './atencion/atender-cita/atender-cita.component';
 import { ReportesComponent } from './atencion/reportes/reportes.component';
-import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogoConfirmacionComponent } from './dialogo-confirmacion/dialogo-confirmacion.component';
 import { RegistrarMotivoComponent } from './Administrador/gestionMotivo/registrar-motivo/registrar-motivo.component';
 import { ConsultarMotivoComponent } from './Administrador/gestionMotivo/consultar-motivo/consultar-motivo.component';
 import { EdicionMotivoComponent } from './Administrador/gestionMotivo/edicion-motivo/edicion-motivo.component';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { RegistrarPoblacionComponent } from './Administrador/gestionPoblacion/registrar-poblacion/registrar-poblacion.component';
 import { ConsultarPoblacionComponent } from './Administrador/gestionPoblacion/consultar-poblacion/consultar-poblacion.component';
 import { EditarPoblacionComponent } from './Administrador/gestionPoblacion/editar-poblacion/editar-poblacion.component';
@@ -45,8 +43,9 @@ import { FiltroPoblacionPipe } from './pipes/filtro-poblacion.pipe';
 import { FiltroMotivoPipe } from './pipes/filtro-motivo.pipe';
 import { FiltroEdadPipe } from './pipes/filtro-edad.pipe';
 import { LoginComponent } from './gestionUsuarios/login/login.component';
-import {MatStepperModule} from '@angular/material/stepper';
+import { MatStepperModule } from '@angular/material/stepper';
 import { AppRoutingModule } from './app-routing.module';
+
 
 
 
@@ -105,7 +104,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
 
   ],
   bootstrap: [AppComponent],
@@ -113,4 +112,5 @@ import { AppRoutingModule } from './app-routing.module';
     DialogoConfirmacionComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- SOFTWARE/ClientApp/src/app/app.server.module.ts
+++ SOFTWARE/ClientApp/src/app/app.server.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
+import { AppModule } from './AppModule';
 
 @NgModule({
     imports: [AppModule, ServerModule, ModuleMapLoaderModule],
