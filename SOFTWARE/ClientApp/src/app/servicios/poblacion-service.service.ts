b83a35bb491f282a52173d79012de10469de0b9f
javier-camilo <camiloviloriatovar@gmail.com>
2024-02-22 15:35:37 -0500

    cambios en diseño

--- SOFTWARE/ClientApp/src/app/servicios/poblacion-service.service.ts
+++ SOFTWARE/ClientApp/src/app/servicios/poblacion-service.service.ts
@@ -23,7 +23,7 @@ export class PoblacionServiceService {
 
   constructor(private http: HttpClient,
     private handleErrorService: HandleHttpErrorService) {
-      this.baseUrl='https://turno.azurewebsites.net/';
+      this.baseUrl='https://localhost:7240/';
 
   }
 
